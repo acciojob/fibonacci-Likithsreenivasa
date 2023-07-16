c23b15fb844c8329dc9b60f3a4180155d6f8a597
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-07-16 17:06:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,25 @@
 function fibonacci(num) {
 // your code here
-	let sum=0;
-	if(num <0 || num > 50)
-	{
-		return null;
-	}
-	else if(num==0)
+	if(num==0)
 	{
 		return 0;
 	}
-	else if (num==1) {
+	else if(num==1)
+	{
 		return 1;
 	}
-	let prev1=0;
-	let prev2=1;
-	else
-	{
-	for(let i=2;i<=num;i++)
-		{
-			sum=prev1+prev2;
-			prev1=prev2;
-			prev2=current;
-		}
-		return prev2;
+	else{
+		prev1=0;
+		prev2=1;
+		let sum=0;
+		for(let i=0;i<num;i++)
+			{
+				sum=prev1+prev2;
+				prev1=prev2;
+				prev2=sum;
+			}
 	}
+	return sum;
 	
 }
 
