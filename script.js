e45a82afdb521522d4ae1515118dab5afc7494b9
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-07-16 17:27:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 function fibonacci(num) {
 // your code here
-	if (num === 0) {
-    return 0;
-  } 
-	else if (num === 1) {
-    return 1;
-  } 
-	else {
-    var a = 0;
-    var b = 1;
-    var result;
-
-    for (var i = 2; i <= num; i++) {
-      result = a + b;
-      a = b;
-      b = result;
-    }
-
-    return result;
-  }
+	if(num==0)
+	{
+		return 0;
+	}
+	else if(num==1)
+	{
+		return 1;
+	}
+	else
+	{
+		let prev1=0;
+		let prev2=1;
+		let result=0;
+		for(let i=2;i<=n;i++)
+			{
+			result=prev1+prev2;
+				prev1=prev2;
+				prev2=result;
+			}
+		return result;
+	}
 	
 }
 
